b96a718c8d4592b39cee2050649a680480ced037
FreePeter <wenlei.xie@gmail.com>
2013-04-18 14:52:20 -0400

    Fix bug in datatype2

--- LearningMPI/mpi_datatype2.cpp
+++ LearningMPI/mpi_datatype2.cpp
@@ -14,14 +14,21 @@ int main(int argc, char **argv) {
 
     //  Define a MPI type for pair<int, double>
     int blocklens[2] = {1, 1};
-    MPI_Aint offsets[2] = {0, sizeof(int)};
+    pair<int, double> temp;
+
+    MPI_Aint firstAddr, secondAddr;
+    MPI_Get_address(&temp.first, &firstAddr);
+    MPI_Get_address(&temp.second, &secondAddr);
+
+    MPI_Aint offsets[2] = {0, secondAddr - firstAddr};
     MPI_Datatype old_types[2] = {MPI_INT, MPI_DOUBLE};
+
     MPI_Datatype MPI_PAIR_INT_DOUBLE;
-    MPI_Type_struct(2, blocklens, offsets, old_types, &MPI_PAIR_INT_DOUBLE);
+    MPI_Type_create_struct(2, blocklens, offsets, old_types, &MPI_PAIR_INT_DOUBLE);
     MPI_Type_commit(&MPI_PAIR_INT_DOUBLE);
 
     if (worldRank != 0) {
-        pair<int, double> msg(worldRank, worldSize / 10);
+        pair<int, double> msg(worldRank, worldSize / 10.0);
         MPI::COMM_WORLD.Send(&msg, 1 /* count */, MPI_PAIR_INT_DOUBLE, 0 /* dest */, MsgTag);
     } else {
         printf("Main node !\n");
