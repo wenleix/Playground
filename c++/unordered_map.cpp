8a0f67db609d16e51d487c0ae09a046e3f665f1b
Wenlei Xie <wenlei.xie@gmail.com>
2013-07-06 23:32:15 -0700

    Add test for unordered_map

---
+++ c++/unordered_map.cpp
@@ -0,0 +1,20 @@
+//  Test unordered_map in C++0x
+//  For GCC, it requires to be compiled with flag -std=c++0x
+
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    vector< pair<int, int> > t;
+    t.push_back(make_pair(1, 2));
+    t.push_back(make_pair(15, 4));
+
+    unordered_map<int, int> q(t.begin(), t.end());
+    for (auto& x: q)
+        cout << x.first << ":" << x.second << endl;
+    return 0;
+}
+
